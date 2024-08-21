c6f399cc57b3218b27ac4761f28def33bc50aba0
Max Wilk <max@rachelwilk.com>
2024-08-21 02:53:14 -0400

    initial commit

---
+++ maven-test-library/src/main/java/com/maxwilk/maven_test_library/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.maxwilk.maven_test_library;
+
+import android.util.Log;
+
+public class HelloWorld {
+
+    public static void SayHelloWorld() {
+        Log.d("MavenTestLibrary", "Hello World");
+    }
+
+}
